0b1cbc46906e8edb8d1fbe63908d8d5ceac0e041
hamke <the235style@yahoo.com>
2019-08-26 07:31:34 +0900

    Site updated: 2019-08-26 07:31:33

---
+++ js/back-to-top.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(window).scroll(function () {
+       if ($(this).scrollTop() > 50) {
+         $('#chat-button').fadeIn();
+         $('#back-to-top').fadeIn();
+         $('#footer-banner').fadeIn();
+       } else {
+         $('#chat-button').fadeOut();
+         $('#back-to-top').fadeOut();
+         $('#footer-banner').fadeOut();
+       }
+     });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+      // $('#back-to-top').tooltip('hide');
+      $('body,html').animate({
+        scrollTop: 0
+      }, 800);
+      return false;
+    });
+    // $('#back-to-top').tooltip('show');
+});
