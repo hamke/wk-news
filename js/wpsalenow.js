adaff23581f9f89680581348bbe05be1a7907a9e
Li Xianghao <li_xianghao@YiMac.local>
2020-11-03 05:33:05 +0900

    Site updated: 2020-11-03 05:33:00

--- js/wpsalenow.js
+++ js/wpsalenow.js
@@ -5,8 +5,9 @@ const plugins_envato_sale = 'https://www.wpsalenow.com/api/envato/plugins/sale/'
 fetch(themes_envato_free)
 .then(res => res.json())
 .then(wptalk_themes_free_result => {
+  // console.log(wptalk_themes_free_result);
   if ( wptalk_themes_free_result !== null && wptalk_themes_free_result !== '') {
-    let output_themes_free = '<h2>◼︎ 워드프레스 테마 실시간 프로모션 (무료)</h2>';
+    let output_themes_free = '';
     for (let t in wptalk_themes_free_result) {
       id = Number(t) + 1;
       if ( t < wptalk_themes_free_result ) {
@@ -15,8 +16,10 @@ fetch(themes_envato_free)
         '<li><a href="' + wptalk_themes_free_result[t].url + '" target="_blank" class="">' + wptalk_themes_free_result[t].name + '</a> ( <s>' + wptalk_themes_free_result[t].price + '</s> -> <b>0.00 USD</b> )</li>';
       }
     }
-    output_themes_free += '<hr>';
-    document.getElementById('wpsalenow-themes-free').innerHTML = output_themes_free;
+    // console.log(output_themes_free);
+    if ( output_themes_free !== '' ) {
+      document.getElementById('wpsalenow-themes-free').innerHTML = '<h2>◼︎ 워드프레스 테마 실시간 프로모션 (무료)</h2>' + output_themes_free + '<hr>';
+    }
   }
 })
 
@@ -25,7 +28,7 @@ fetch(themes_envato_sale)
 .then(wptalk_themes_sale_result => {
   // console.log(wptalk_themes_sale_result);
   if ( wptalk_themes_sale_result !== null && wptalk_themes_sale_result !== '') {
-    let output_themes_sale = '<h2>◼︎ 워드프레스 테마 실시간 프로모션 (할인)</h2>';
+    let output_themes_sale = '';
     for (let t in wptalk_themes_sale_result) {
       id = Number(t) + 1;
       if ( t < wptalk_themes_sale_result ) {
@@ -34,8 +37,10 @@ fetch(themes_envato_sale)
         '<li><a href="' + wptalk_themes_sale_result[t].url + '" target="_blank" class="">' + wptalk_themes_sale_result[t].name + '</a> ( <b>' + wptalk_themes_sale_result[t].price_sale + ' USD</b> )</li>';
       }
     }
-    output_themes_sale += '<hr>';
-    document.getElementById('wpsalenow-themes-sale').innerHTML = output_themes_sale;
+    // console.log(output_themes_sale);
+    if ( output_themes_sale !== '' ) {
+      document.getElementById('wpsalenow-themes-sale').innerHTML = '<h2>◼︎ 워드프레스 테마 실시간 프로모션 (할인)</h2>' + output_themes_sale + '<hr>';
+    }
   }
 })
 
@@ -44,7 +49,7 @@ fetch(plugins_envato_sale)
 .then(wptalk_plugins_sale_result => {
   // console.log(wptalk_plugins_sale_result);
   if ( wptalk_plugins_sale_result !== null && wptalk_plugins_sale_result !== '') {
-    let output_plugins_sale = '<h2>◼︎ 워드프레스 플러그인 실시간 프로모션 (할인)</h2>';
+    let output_plugins_sale = '';
     for (let t in wptalk_plugins_sale_result) {
       id = Number(t) + 1;
       if ( t < wptalk_plugins_sale_result ) {
@@ -53,7 +58,9 @@ fetch(plugins_envato_sale)
         '<li><a href="' + wptalk_plugins_sale_result[t].url + '" target="_blank" class="">' + wptalk_plugins_sale_result[t].name + '</a> ( <b>' + wptalk_plugins_sale_result[t].price_sale + ' USD</b> )</li>';
       }
     }
-    output_plugins_sale += '<hr>';
-    document.getElementById('wpsalenow-plugins-sale').innerHTML = output_plugins_sale;
+    // console.log(output_plugins_sale);
+    if ( output_plugins_sale !== '' ) {
+      document.getElementById('wpsalenow-plugins-sale').innerHTML = '<h2>◼︎ 워드프레스 플러그인 실시간 프로모션 (할인)</h2>' + output_plugins_sale + '<hr>';
+    }
   }
 })
